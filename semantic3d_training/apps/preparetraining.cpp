7dcdc3166808c643a9236fcfceff76519f0d5f86
Julius Adorf <jeadorf@gmail.com>
2011-03-16 17:22:32 +0100

    fixed .gitignore

--- semantic3d_training/apps/preparetraining.cpp
+++ semantic3d_training/apps/preparetraining.cpp
@@ -4,6 +4,9 @@
  * Author: Julius Adorf 
  */
 
+#include <iostream>
+
+using namespace std;
 
 /** \brief Prepares training data for the feature detection and extraction
  * stage in the training pipeline of ROS package tod_training.
@@ -13,6 +16,24 @@ int main(int argc, char *argv[])
     // Input:
     // - training base directory
     // - directory with all training data for one specific object
+    
+    // TODO: load options from command line using boost::program_options
+    // Options opts;
+    // options(argc, argv, opts);
+
+    if (argc != 3)
+    {
+        cerr << "Usage: preparetraining <bags-dir> <base-dir>" << endl;
+        return 1;
+    }
+
+    // This directory contains all the data from TUM/IAS semantic3d database.
+    string bags_dir(argv[1]);
+    // This directory is the training base that is to be prepared. 
+    string train_dir(argv[2]);
+
+    cout << bags_dir << endl;
+    cout << train_dir << endl;
 
     // list images
     // for v in views do
