83f28dfbe2c802c28a6ad9c2b73183df850272ab
Julius Adorf <jeadorf@gmail.com>
2011-03-16 16:12:50 +0100

    create executable

---
+++ semantic3d_training/apps/preparetraining.cpp
@@ -0,0 +1,14 @@
+/*
+ * preparetraining.cpp
+ *
+ * Author: Julius Adorf 
+ */
+
+
+/** \brief Prepares training data for the feature detection and extraction
+ * stage in the training pipeline of ROS package tod_training.
+ */
+int main(int argc, char *argv[])
+{
+
+}
