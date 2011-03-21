ae4e5191fe796664bf8f58ccf51cb288d68b2147
Julius Adorf <jeadorf@gmail.com>
2011-03-21 13:56:19 +0100

    added point cloud segmentation test

--- semantic3d_training/test/test_pcl.cpp
+++ semantic3d_training/test/test_pcl.cpp
@@ -9,6 +9,14 @@
 #include "cv.h"
 #include <opencv2/highgui/highgui.hpp>
 
+#include "fiducial/fiducial.h"
+#include "tod/core/Features3d.h"
+#include "tod/training/masking.h"
+#include "tod/training/clouds.h"
+#include "tod/training/Opts.h"
+#include "opencv_candidate/Camera.h"
+
+
 using namespace std;
 using namespace pcl;
 
@@ -26,27 +34,28 @@ TEST(PCL, ReadSemantic3dPointCloud) {
     io::loadPCDFile("./data/sample.delimited.pcd", cloud);
 }
 
-/** Test how points can be projected onto two out of three coordinates. */
+/** Test how points can be projected onto two out of three coordinates. Use the
+ * characteristically shaped icetea2 object and project the y and z coordinates
+ * onto a picture. */
 TEST(PCL, CoordinateProjection) {
     using namespace cv;
     // TODO: remove platform dependency
     typedef unsigned char uint8;
     PointCloud<PointXYZ> cloud;
-    //io::loadPCDFile("./data/sample.delimited.pcd", cloud);
-    io::loadPCDFile("/home/julius/Studium/BA/clutter-segmentation/objrec2d/build/semantic-3d/icetea2.delimited.pcd/icetea2_0_.log.delimited.pcd", cloud);
+    io::loadPCDFile("./data/sample.delimited.pcd", cloud);
 
     // find y and z range
-    float zmin = numeric_limits<float>::max();
-    float zmax = numeric_limits<float>::min();
     float ymin = numeric_limits<float>::max();
     float ymax = numeric_limits<float>::min();
+    float zmin = numeric_limits<float>::max();
+    float zmax = numeric_limits<float>::min();
     PointCloud<PointXYZ>::iterator it = cloud.begin();
     PointCloud<PointXYZ>::iterator end = cloud.end();
     while (it != end) {
-        zmin = min(zmin, it->z);
-        zmax = max(zmax, it->z);
         ymin = min(ymin, it->y);
         ymax = max(ymax, it->y);
+        zmin = min(zmin, it->z);
+        zmax = max(zmax, it->z);
         it++;
     }
     // create image and scale of 2d coordinate axes
@@ -66,5 +75,21 @@ TEST(PCL, CoordinateProjection) {
     namedWindow("TEST(PCL, CoordinateProjection)"); 
     imshow("TEST(PCL, CoordinateProjection)", img);
     waitKey(5000);
+}
+
+/** Test how to create a mask by doing a perspective projection */
+TEST(PCL, PerspectiveProjection) {
+    fiducial::KnownPoseEstimator pose_est("./data/fat_free_milk_image_00000.png.pose.yaml");
+    cv::Mat colorimg = cv::imread("./data/fat_free_milk_image_00000.png", CV_LOAD_IMAGE_COLOR);
+    tod::Camera camera = tod::Camera("./data/fat_free_milk_camera.yml", opencv_candidate::Camera::TOD_YAML);
+    tod::Features2d f2d(camera, colorimg);
+    f2d.camera.pose = pose_est.estimatePose(cv::Mat());
+    PointCloud<PointXYZRGB> cloud;
+    io::loadPCDFile("./data/fat_free_milk_cloud_00000.pcd", cloud);
+    f2d.mask = tod::cloudMask(cloud, f2d.camera.pose, camera);
+    cv::Mat colorMask;
+    cv::cvtColor(f2d.mask, colorMask, CV_GRAY2BGR);
+    cv::imshow("mask", f2d.image & colorMask);
+    cv::waitKey(5000);
 }
 
