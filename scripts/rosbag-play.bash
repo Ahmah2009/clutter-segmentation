59c0f242e9f6dd7b01479a4c8ccc1ab08e4ec4ba
Julius Adorf <jeadorf@gmail.com>
2011-03-30 22:26:47 +0200

    refactored helper bash scripts

---
+++ scripts/rosbag-play.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [ "$1" = "--help" ] || [ ! "$1" ] ; then
+    cat <<HELP
+Usage: rosbag-play.bash <training-subject>
+
+Replays a training bag for a certain training subject in the tod kinect
+training dataset using rosbag. Replay must be started by hitting <space> in the
+terminal.
+HELP
+    exit
+fi
+
+if [ ! "$CLUTSEG_PATH" ] ; then
+    echo "ERROR: Environment variable CLUTSEG_PATH is not defined."
+    exit
+fi
+
+if [ ! "$(pgrep rosout)" ] ; then
+    echo "WARNING: Master server roscore does not seem to be running."
+fi 
+
+rosbag play --pause --rate=0.05 --loop $CLUTSEG_PATH/$1.bag $CLUTSEG_PATH/$1.tf.bag
+
--- scripts/rviz-show-pcl.bash
+++ scripts/rviz-show-pcl.bash
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
-#
-# TODO: load valid configuration into rviz
+
 # TODO: decide whether this is helpful for Dejan
-# TODO: test
-# TODO: fix broken paths
-# TODO: make use of exported environment variable to fix path issues
 # TODO: find one single term to fix problems with name of training data and training base
+
+if [ "$1" = "--help" ] then
+    cat <<HELP
+Usage: rviz-show-pcl.bash
+
+Plays a training bag for the downy training subject in the tod kinect training
+dataset using rosbag and shows everything in rviz. No further configuration is
+necessary. Replay must be started by hitting <space> in the terminal.
+HELP
+    exit
+fi
+
+
 if [ ! "$CLUTSEG_PATH" ] ; then
     echo "ERROR: Environment variable CLUTSEG_PATH is not defined."
     exit
