7ff06c2b8d9c7d6c304b684a69ba18933cb6bcc8
Julius Adorf <jeadorf@gmail.com>
2011-03-31 09:21:22 +0200

    add scripts for s3dbase-single experiment

---
+++ scripts/s3dbase-single-extract-features.bash
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [ "$1" = "--help" ] ; then
+    cat <<HELP
+Usage: s3dbase-single-extract-features.bash <training-subject>
+
+Extracts features using tod_training detector and f3dcreator, thus running the
+last two steps of the tod_training pipeline without estimating poses or
+creating masks.
+HELP
+    exit
+fi
+
+if [ ! "$CLUTSEG_PATH" ] ; then
+    echo "ERROR: Environment variable CLUTSEG_PATH is not defined."
+    exit
+fi
+
+
+#!/bin/bash
+if [ ! -e $1/camera.yml ];then
+	echo "ERROR: Data for training subject not found."
+	exit
+fi
+
+echo "detector"
+rosrun tod_training detector -d $1 --verbose=true
+echo "f3d"
+rosrun tod_training f3d_creator -d $1 --verbose=true
+
