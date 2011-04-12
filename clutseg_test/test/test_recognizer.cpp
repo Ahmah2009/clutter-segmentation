c16bd7b2c76bffa573666010c1665168f228b6a2
Julius Adorf <jeadorf@gmail.com>
2011-04-12 14:52:43 +0200

    validating assumption on number of keypoint matches added to test_recognizer.cpp

--- clutseg_test/test/test_recognizer.cpp
+++ clutseg_test/test/test_recognizer.cpp
@@ -22,12 +22,12 @@
 #include <set>
 
 #define foreach BOOST_FOREACH
-typedef std::pair < int, int >idx_pair_t;
 using std::list;
 using std::string;
 using std::istream;
 using namespace cv;
 using namespace tod;
+using namespace std;
 
 namespace po = boost::program_options;
 
@@ -74,17 +74,13 @@ TEST(Recognizer, TestRun)
     opts.baseDirectory = p + "/ias_kinect_train";
     opts.imageFile = p + "/ias_kinect_train/haltbare_milch/image_00000.png";
     opts.config = p + "/ias_kinect_train/config.yaml";
-    opts.verbose = 2;
-    opts.mode = 1;
+    opts.verbose = 0;
+    opts.mode = KINECT;
 
     FileStorage fs;
     fs = FileStorage(opts.config, FileStorage::READ);
     opts.params.read(fs[TODParameters::YAML_NODE_NAME]);
     
-    // Validate assumptions on option values
-    EXPECT_EQ(KINECT, opts.mode);
-    EXPECT_EQ(2, opts.verbose);
-
     tod::Loader loader(opts.baseDirectory);
     vector < cv::Ptr < TexturedObject > >objects;
     loader.readTexturedObjects(objects);
@@ -124,7 +120,7 @@ TEST(Recognizer, TestRun)
         cout << boost::format("Mapping objects[%d] to object id %d") % i % (*objectIds.find(i)) << endl;
     }
 
-    Ptr < FeatureExtractor > extractor =
+    Ptr<FeatureExtractor> extractor =
         FeatureExtractor::create(opts.params.feParams);
     Features2d test;
     // [julius] The image is read in grayscale, see OpenCV documentation-
@@ -135,44 +131,50 @@ TEST(Recognizer, TestRun)
     std::cout << "Extracted " << test.keypoints.
         size() << " points" << std::endl;
 
-    // [julius] The naming shows that the programmer inteded to use the
-    // RatioTestMatcher though the used Matcher actually depends on the
-    // parameter configuration and as such the name is rather confusing. 
-    cv::Ptr < Matcher > rtMatcher =
-        Matcher::create(opts.params.matcherParams);
+    cv::Ptr<Matcher> matcher = Matcher::create(opts.params.matcherParams);
     // [julius] This adds all keypoint descriptors from the training images to
     // the matcher. See OpenCV's DescriptorMatcher.
-    rtMatcher->add(base);
+    matcher->add(base);
 
-    cv::Ptr < Recognizer > recognizer;
+    cv::Ptr<Recognizer> recognizer;
     if (opts.mode == TOD) {
         recognizer =
-            new TODRecognizer(&base, rtMatcher, &opts.params.guessParams,
+            new TODRecognizer(&base, matcher, &opts.params.guessParams,
                               opts.verbose, opts.baseDirectory,
                               opts.params.clusterParams.maxDistance);
     } else if (opts.mode == KINECT) {
         recognizer =
-            new KinectRecognizer(&base, rtMatcher, &opts.params.guessParams,
+            new KinectRecognizer(&base, matcher, &opts.params.guessParams,
                                  opts.verbose, opts.baseDirectory);
     } else {
         std::cout << "Invalid mode option!" << std::endl;
         ASSERT_TRUE(false);
     }
 
-    vector < tod::Guess > foundObjects;
+    vector < Guess > guesses;
     // [julius] This is the key function call in this whole main method.
-    recognizer->match(test, foundObjects);
+    recognizer->match(test, guesses);
 
-    foreach(const tod::Guess & guess, foundObjects) {
-        std::cout << "Object name = " << guess.getObject()->
-            name << ", imageId = ";
+    // [julius] Assume that there are about 10*<number of test keypoints>
+    // matches found.
+    size_t totalMatchCnt = 0;
+    foreach(int objectId, objectIds) {
+        vector<DMatch> matches;
+        matcher->getObjectMatches(objectId, matches);
+        totalMatchCnt += matches.size();
+    }
+    EXPECT_LT(6 * test.keypoints.size(), totalMatchCnt);
+    EXPECT_LT(totalMatchCnt, 10 * test.keypoints.size());
+    
+    foreach(const Guess & guess, guesses) {
+        cout << "Object name = " << guess.getObject()->name << ", imageId = ";
         // Get the used image indices
-        std::set < unsigned int >image_indices;
+        set < unsigned int >image_indices;
         BOOST_FOREACH(unsigned int inlier_index, guess.inliers)
             image_indices.insert(guess.image_indices_[inlier_index]);
         BOOST_FOREACH(unsigned int image_index, image_indices)
-            std::cout << image_index << " ";
-        std::cout << std::endl;
+            cout << image_index << " ";
+        cout << std::endl;
     }
 
 }
