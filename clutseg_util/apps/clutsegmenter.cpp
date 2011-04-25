a757469ff478000189ac86458daf5784ebf3eaee
Julius Adorf <jeadorf@gmail.com>
2011-04-25 17:06:37 +0200

    make interface visible

--- clutseg_util/apps/clutsegmenter.cpp
+++ clutseg_util/apps/clutsegmenter.cpp
@@ -7,13 +7,43 @@
 #include "pose_util.h"
 
 #include <pcl/io/pcd_io.h>
-#include <tod/detecting/Loader.h>
 #include <stdlib.h>
 #include <iostream>
 #include <string>
 
 using namespace clutseg;
 using namespace std;
+
+void readInput(int argc, char **argv, Mat & queryImage, PointCloudT & queryCloud) {
+    // Read input
+    if (argc <= 2) {
+        cerr << "Usage: clutsegmenter <query-image> <query-cloud> [<inlier-cloud-out>] [<pose-out>]" << endl;
+        abort();
+    }
+    queryImage = imread(argv[1]);
+    io::loadPCDFile(argv[2], queryCloud);
+    if (queryImage.empty()) {
+        cerr << "Cannot read query image." << endl;
+        abort();
+    }
+}
+
+void processOutput(int argc, char **argv, bool positive, Guess & guess, PointCloudT & inlierCloud) {
+    // Process result
+    if (positive) {
+        cout << "Recognized " << guess.getObject()->name << endl;
+        if (argc >= 4) {
+            io::savePCDFileASCII(argv[3], inlierCloud);
+        }
+        if (argc >= 5) {
+           writePose(argv[4], guess.aligned_pose()); 
+        }
+    } else {
+        cerr << "Could not recognize object." << endl;
+        abort();
+    }
+}
+
 
 int main(int argc, char **argv) {
     // Create segmenter
@@ -26,37 +56,18 @@ int main(int argc, char **argv) {
     Mat queryImage;
     PointCloudT queryCloud;
 
+    readInput(argc, argv, queryImage, queryCloud);
+
     // Output values
     bool positive;
     Guess guess;
     PointCloudT inlierCloud;
 
-    // Read input
-    if (argc <= 2) {
-        cerr << "Usage: clutsegmenter <query-image> <query-cloud> [<inlier-cloud-out>] [<pose-out>]" << endl;
-        return 1;
-    }
-    queryImage = imread(argv[1]);
-    io::loadPCDFile(argv[2], queryCloud);
-    if (queryImage.empty()) {
-        cerr << "Cannot read query image." << endl;
-        return 1;
-    }
-
     // Actual recognition 
     positive = segmenter.recognize(queryImage, queryCloud, guess, inlierCloud);
 
-    if (positive) {
-        cout << "Recognized " << guess.getObject()->name << endl;
-        if (argc >= 4) {
-            io::savePCDFileASCII(argv[3], inlierCloud);
-        }
-        if (argc >= 5) {
-           writePose(argv[4], guess.aligned_pose()); 
-        }
-        return 0;
-    } else {
-        cerr << "Could not recognize object." << endl;
-        return 1;
-    }
+    processOutput(argc, argv, positive, guess, inlierCloud);
+
+    return 0;
 }
+
