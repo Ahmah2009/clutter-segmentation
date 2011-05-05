68e91c74d49f68d7e6da183dc6b7dab858b2f4ec
Julius Adorf <jeadorf@gmail.com>
2011-05-05 18:09:39 +0200

    add tests for surf, dynamicsurf

--- clutseg/test/test_extractor.cpp
+++ clutseg/test/test_extractor.cpp
@@ -2,7 +2,17 @@
  * Author: Julius Adorf
  *
  * This is a test with which I want to find out which of the extractor
- * algorithms can be combined and which of them actually work as expected.
+ * algorithms can be combined and which of them actually work as expected.  The
+ * following aspects have to be evaluated to make them a possible factor in
+ * parameter selection:
+ * - whether keypoints are produced outside of the given mask
+ * - how much time is required to extract features from an image
+ * - whether the combination of (descriptor_type, extractor_type, detector_type)
+ *   leads to the expected configuration
+ * - how much influence do min_features and max_features have on the extraction
+ *   process
+ * - which parameter are influential for each combination of (descriptor_type,
+ *   extractor_type, detector_type)
  */
 
 #include "test.h"
@@ -52,9 +62,6 @@ class ExtractorTest : public ::testing::Test {
             f2d_masked.mask = imread("./data/image_00000.png.mask.png", 0);
 
             {
-                // This configuration has been provided by ethanrublee.
-                // It turns out that max_features is taken into account but
-                // it is not an invariant, just a guideline.
                 orb_harrisfast.detector_type = "ORB";
                 orb_harrisfast.extractor_type = "ORB";
                 orb_harrisfast.descriptor_type = "ORB";
@@ -65,8 +72,6 @@ class ExtractorTest : public ::testing::Test {
             }
 
             {
-                // This is the configuration  I have used in the first
-                // experiments with tod_kinect and ias_kinect training data.
                 dynamicfast_multiscale_rbrief.detector_type = "DynamicFAST";
                 dynamicfast_multiscale_rbrief.extractor_type = "multi-scale";
                 dynamicfast_multiscale_rbrief.descriptor_type = "rBRIEF";
@@ -77,11 +82,6 @@ class ExtractorTest : public ::testing::Test {
             }
 
             {
-                // This is kind of a weird configuration I have evaluated very
-                // quickly without much thinking. It most probably does not
-                // make sense to wrap SIFT with yet another multiscale
-                // extractor. Anyways, for sake of understanding, see what 
-                // happens...
                 sift_multiscale_rbrief.detector_type = "SIFT";
                 sift_multiscale_rbrief.extractor_type = "multi-scale";
                 sift_multiscale_rbrief.descriptor_type = "rBRIEF";
@@ -92,10 +92,6 @@ class ExtractorTest : public ::testing::Test {
             }
  
             {
-                // This is a new configuration (2011-05-04) I haven't tried
-                // before on any training sense. It seems to make sense to me
-                // though and there is the question where it really detects
-                // keypoints only within the mask.
                 sift_sequential_rbrief.detector_type = "SIFT";
                 sift_sequential_rbrief.extractor_type = "sequential";
                 sift_sequential_rbrief.descriptor_type = "rBRIEF";
@@ -104,9 +100,29 @@ class ExtractorTest : public ::testing::Test {
                 sift_sequential_rbrief.detector_params["min_features"] = 500;
                 sift_sequential_rbrief.detector_params["max_features"] = 700;
             } 
+ 
+            {
+                surf_sequential_rbrief.detector_type = "SURF";
+                surf_sequential_rbrief.extractor_type = "sequential";
+                surf_sequential_rbrief.descriptor_type = "rBRIEF";
+
+                surf_sequential_rbrief.extractor_params["octaves"] = 3;
+                surf_sequential_rbrief.detector_params["min_features"] = 600;
+                surf_sequential_rbrief.detector_params["max_features"] = 900;
+            }
+ 
+            {
+                dynamic_surf_sequential_rbrief.detector_type = "SURF";
+                dynamic_surf_sequential_rbrief.extractor_type = "sequential";
+                dynamic_surf_sequential_rbrief.descriptor_type = "rBRIEF";
+
+                dynamic_surf_sequential_rbrief.extractor_params["octaves"] = 3;
+                dynamic_surf_sequential_rbrief.detector_params["min_features"] = 600;
+                dynamic_surf_sequential_rbrief.detector_params["max_features"] = 900;
+            }
         }
 
-        void expectMaskingWorks(const FeatureExtractionParams & params) {
+        void expectMaskingWorks(const FeatureExtractionParams & params, bool expect = true) {
             Ptr<FeatureExtractor> extractor = FeatureExtractor::create(params);
             extractor->detectAndExtract(f2d_masked);
             EXPECT_EQ(CV_8U, f2d_masked.mask.type());
@@ -114,87 +130,181 @@ class ExtractorTest : public ::testing::Test {
             // of interest.
             vector<KeyPoint> outside;
             keypointsOutsideMask(f2d_masked.keypoints, f2d_masked.mask, outside);
-            Mat canvas;
+            /* annoying-la Mat canvas;
             cvtColor(f2d_masked.mask, canvas, CV_GRAY2BGR);
             clutseg::drawKeypoints(canvas, f2d_masked.keypoints, Scalar::all(-1));
             clutseg::drawKeypoints(canvas, outside, Scalar(0, 0, 255));
             imshow("keypoints inside and outside of the mask", canvas);
-            waitKey(-1);
-            EXPECT_EQ(0, outside.size());
+            waitKey(1200); */
+            if (expect) {
+                EXPECT_EQ(0, outside.size());
+            } else {
+                EXPECT_LT(0, outside.size());
+            }
+        }
+
+        void expectUsesMaxFeatures(FeatureExtractionParams params, int min_features, int max_features_high, int max_features_low) {
+            params.detector_params["min_features"] = min_features;
+            params.detector_params["max_features"] = max_features_high;
+            FeatureExtractor::create(params, stats)->detectAndExtract(f2d);
+            int more = f2d.keypoints.size();
+            params.detector_params["max_features"] = max_features_low;
+            f2d.keypoints = vector<KeyPoint>();
+            FeatureExtractor::create(params, stats)->detectAndExtract(f2d);
+            int less = f2d.keypoints.size();
+            EXPECT_GT(more, less);
+            EXPECT_TRUE(stats.pm_max_features_used);
+        }
+
+        void expectUsesMinFeatures(FeatureExtractionParams params, int max_features, int min_features_high, int min_features_low) {
+            params.detector_params["max_features"] = max_features;
+            params.detector_params["min_features"] = min_features_low;
+            FeatureExtractor::create(params, stats)->detectAndExtract(f2d);
+            int less = f2d.keypoints.size();
+            params.detector_params["min_features"] = min_features_high;
+            FeatureExtractor::create(params, stats)->detectAndExtract(f2d);
+            int more = f2d.keypoints.size();
+            EXPECT_LT(less, more);
+            EXPECT_TRUE(stats.pm_min_features_used);
         }
 
         Features2d f2d;
         Features2d f2d_masked;
         detector_stats stats;
+        
+        // This configuration has been provided by ethanrublee.  It turns out
+        // that max_features is taken into account but it is not an invariant,
+        // just a guideline.
         FeatureExtractionParams orb_harrisfast;
+        // This is the configuration  I have used in the first experiments with
+        // tod_kinect and ias_kinect training data.
         FeatureExtractionParams dynamicfast_multiscale_rbrief;
+        // This is kind of a weird configuration I have evaluated very quickly
+        // without much thinking. It most probably does not make sense to wrap
+        // SIFT with yet another multiscale extractor. Anyways, for sake of
+        // understanding, see what happens... See ticket 1044 in OpenCV.
+        // Requires post-processing.
         FeatureExtractionParams sift_multiscale_rbrief;
+        // This is a new configuration (2011-05-04) I haven't tried before on
+        // any training basee. It seems to be a viable option though. Keypoints
+        // are detected outside of the mask, thus this requires post-processing
+        // in tod_training. See ticket 1044 in OpenCV.
         FeatureExtractionParams sift_sequential_rbrief;
+        // This is a new configuration (2011-05-04) I haven't tried it before
+        // on any training base.
+        FeatureExtractionParams surf_sequential_rbrief;
+        // This is a new configuration (2011-05-04) I haven't tried it before
+        // on any training base.
+        FeatureExtractionParams dynamic_surf_sequential_rbrief;
 };
 
 
-TEST_F(ExtractorTest, OrbExtractorHarrisFastStats) {
-    detector_stats stats;
+// Check which combinations are definitely influenced by min_features 
+// ---------------------------------------------------------------------------
 
-    Ptr<FeatureExtractor> extractor = FeatureExtractor::create(orb_harrisfast, stats);
-    extractor->detectAndExtract(f2d);
-
-    EXPECT_EQ("<none>", stats.internal_detector);
-    EXPECT_EQ("<none>", stats.internal_extractor);
-    EXPECT_EQ("tod::OrbExtractor with HarrisFast", stats.extractor);
+TEST_F(ExtractorTest, orb_harrisfast_uses_max_features) {
+    expectUsesMaxFeatures(orb_harrisfast, 300, 1000, 300);
 }
 
-TEST_F(ExtractorTest, OrbExtractorHarrisFastNumberOfKpts) {
-    Ptr<FeatureExtractor> extractor = FeatureExtractor::create(orb_harrisfast, stats);
-    extractor->detectAndExtract(f2d);
-    // The guideline keypoints.size() > min_features will be met by the
-    // extractor on the given example image.
-    EXPECT_GE(f2d.keypoints.size(), orb_harrisfast.detector_params["min_features"]);
-    // It does not meet the guideline for the maximum number of features though.
-    EXPECT_GE(f2d.keypoints.size(), orb_harrisfast.detector_params["max_features"]);
-    EXPECT_EQ(1267, f2d.keypoints.size());
+TEST_F(ExtractorTest, dynamicfast_multiscale_rbrief_uses_max_features) {
+    expectUsesMaxFeatures(dynamicfast_multiscale_rbrief, 0, 1000, 100);
 }
 
-TEST_F(ExtractorTest, OrbExtractorHarrisFastZero) {
-    orb_harrisfast.detector_params["max_features"] = 0;
-    Ptr<FeatureExtractor> extractor = FeatureExtractor::create(orb_harrisfast, stats);
-    extractor->detectAndExtract(f2d);
-    // This just proves that the max_features parameter is actually taken into
-    // account here.
-    EXPECT_EQ(f2d.keypoints.size(), 0);
+TEST_F(ExtractorTest, dynamic_surf_sequential_rbrief_uses_max_features) {
+    expectUsesMaxFeatures(dynamic_surf_sequential_rbrief, 0, 1000, 100);
 }
+
+// Check which combinations are definitely influenced by min_features. Whether
+// a combination does not use the min_feature parameter cannot be decided.
+// ---------------------------------------------------------------------------
+
+TEST_F(ExtractorTest, dynamicfast_multiscale_rbrief_uses_min_features) {
+    expectUsesMinFeatures(dynamicfast_multiscale_rbrief, 1200, 1000, 500);
+}
+
+TEST_F(ExtractorTest, dynamic_surf_sequential_rbrief_uses_min_features) {
+    expectUsesMinFeatures(dynamic_surf_sequential_rbrief, 1200, 1000, 500);
+}
+
+
+// Check whether the mask parameter is correctly used
+// ---------------------------------------------------------------------------
 
 TEST_F(ExtractorTest, orb_harrisfast_masking_works) {
     expectMaskingWorks(orb_harrisfast);
 }
 
 TEST_F(ExtractorTest, dynamicfast_multiscale_rbrief_masking_works) {
-    // This is a bug in OpenCV. The masking parameter is useless.
     expectMaskingWorks(dynamicfast_multiscale_rbrief);
 }
 
-TEST_F(ExtractorTest, sift_multiscale_rbrief_stats) {
-    Ptr<FeatureExtractor> extractor = FeatureExtractor::create(sift_multiscale_rbrief, stats);
+TEST_F(ExtractorTest, sift_multiscale_rbrief_masking_works) {
+    // expect to fail because of OpenCV 1044
+    expectMaskingWorks(sift_multiscale_rbrief, false);
+}
+
+TEST_F(ExtractorTest, sift_sequential_rbrief_masking_works) {
+    // expect to fail because of OpenCV 1044
+    expectMaskingWorks(sift_sequential_rbrief, false);
+}
+
+TEST_F(ExtractorTest, surf_sequential_rbrief_masking_works) {
+    expectMaskingWorks(surf_sequential_rbrief);
+}
+
+TEST_F(ExtractorTest, dynamic_surf_sequential_rbrief_masking_works) {
+    expectMaskingWorks(dynamic_surf_sequential_rbrief);
+}
+
+// Check whether expectations on the resulting configuration are met
+// ---------------------------------------------------------------------------
+
+TEST_F(ExtractorTest, orb_harrisfast_config) {
+    FeatureExtractor::create(orb_harrisfast, stats);
+    EXPECT_EQ("<none>", stats.internal_detector);
+    EXPECT_EQ("<none>", stats.internal_extractor);
+    EXPECT_EQ("tod::OrbExtractor with HarrisFast", stats.extractor);
+}
+
+TEST_F(ExtractorTest, dynamicfast_multiscale_rbrief_config) {
+    FeatureExtractor::create(dynamicfast_multiscale_rbrief, stats);
+    cout << stats.internal_detector << endl;
+    EXPECT_EQ("cv::DynamicAdaptedFeatureDetector with tod::RBriefFastAdjuster", stats.internal_detector);
+    EXPECT_EQ("rbrief::RBriefDescriptorExtractor", stats.internal_extractor);
+    EXPECT_EQ("tod::MultiscaleExtractor", stats.extractor);
+}
+ 
+TEST_F(ExtractorTest, sift_multiscale_rbrief_config) {
+    FeatureExtractor::create(sift_multiscale_rbrief, stats);
     EXPECT_TRUE(stats.internal_detector.find("SiftFeatureDetector") != string::npos);
     EXPECT_EQ("rbrief::RBriefDescriptorExtractor", stats.internal_extractor);
     EXPECT_EQ("tod::MultiscaleExtractor", stats.extractor);
 }
 
-TEST_F(ExtractorTest, sift_multiscale_rbrief_masking_works) {
-    // This is a bug in OpenCV. The masking parameter is useless.
-    expectMaskingWorks(sift_multiscale_rbrief);
-}
-
-TEST_F(ExtractorTest, sift_sequential_rbrief_stats) {
-    Ptr<FeatureExtractor> extractor = FeatureExtractor::create(sift_sequential_rbrief, stats);
+TEST_F(ExtractorTest, sift_sequential_rbrief_config) {
+    FeatureExtractor::create(sift_sequential_rbrief, stats);
     EXPECT_TRUE(stats.internal_detector.find("SiftFeatureDetector") != string::npos);
     EXPECT_EQ("rbrief::RBriefDescriptorExtractor", stats.internal_extractor);
     EXPECT_EQ("tod::SequentialExtractor", stats.extractor);
 }
 
-TEST_F(ExtractorTest, sift_sequential_rbrief_masking_works) {
-    expectMaskingWorks(sift_sequential_rbrief);
+TEST_F(ExtractorTest, surf_sequential_rbrief_config) {
+    FeatureExtractor::create(surf_sequential_rbrief, stats);
+    EXPECT_TRUE(stats.internal_detector.find("SurfFeatureDetector") != string::npos);
+    EXPECT_EQ("rbrief::RBriefDescriptorExtractor", stats.internal_extractor);
+    EXPECT_EQ("tod::SequentialExtractor", stats.extractor);
 }
+
+TEST_F(ExtractorTest, dynamic_surf_sequential_rbrief_config) {
+    FeatureExtractor::create(dynamic_surf_sequential_rbrief, stats);
+    EXPECT_TRUE("cv::DynamicAdaptedFeatureDetector with cv::SurfAdjuster", stats.internal_detector);
+    EXPECT_EQ("rbrief::RBriefDescriptorExtractor", stats.internal_extractor);
+    EXPECT_EQ("tod::SequentialExtractor", stats.extractor);
+}
+
+
+// Miscellaneous other tests
+// ---------------------------------------------------------------------------
 
 TEST_F(ExtractorTest, SiftFeatureDetectorIgnoresMask) {
     // I expect having found an annoying bug in OpenCV
