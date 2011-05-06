9ad3ab219f17a199e90e8792204331ba9f11c4a6
Julius Adorf <jeadorf@gmail.com>
2011-05-06 15:06:36 +0200

    improved backport of OpenCV

--- scripts/mods-link.bash
+++ scripts/mods-link.bash
@@ -2,10 +2,6 @@
 for mod in $(cat $CLUTSEG_PATH/clutter-segmentation/mods/mods.txt) ; do
     src=$CLUTSEG_PATH/clutter-segmentation/mods/$mod
     dst=$CLUTSEG_PATH/$mod 
-    if [ -f $dst ] ; then
-        echo "Skipping $mod, destination exists."
-    else
-        ln -v $src $dst
-    fi
+    ln -v -f $src $dst
 done
 
