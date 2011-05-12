2df003d8f2a51a4065036ebba53fc03e9acc6193
Julius Adorf <jeadorf@gmail.com>
2011-05-12 21:11:15 +0200

    fixed sqlite test, now valuable as tutorial

--- clutseg/test/test_sqlite.cpp
+++ clutseg/test/test_sqlite.cpp
@@ -2,6 +2,7 @@
  * Author: Julius Adorf
  */
 
+#include <boost/filesystem.hpp>
 #include <gtest/gtest.h>
 #include <iostream>
 #include <sqlite3.h>
@@ -9,83 +10,67 @@
  
 using namespace std;
 
-// TODO: move to clutseg_test
- 
-TEST(SqliteTest, OpenDB) {
-    sqlite3* db;
-    string Path = "build/OpenDBtest.sqlite3";
-    if(sqlite3_open(Path.c_str(), &db) != SQLITE_OK)
-    {
-      cerr << sqlite3_errmsg(db) << endl;
-    }
-    sqlite3_close(db);
-}
- 
 TEST(SqliteTest, CRUD) {
     sqlite3* db;
-    sqlite3_open("build/test.sqlite3", &db);
+    string fn = "build/SqliteTest.CRUD.sqlite3";
+    boost::filesystem::remove(fn);
+    EXPECT_EQ(SQLITE_OK, sqlite3_open(fn.c_str(), &db));
+
+    {
+        sqlite3_stmt* table;
+        EXPECT_EQ(SQLITE_OK, sqlite3_prepare_v2(db,
+            "CREATE TABLE simple ("
+            "   id INTEGER PRIMARY KEY,"
+            "   text TEXT,"
+            "   fpnum REAL,"
+            "   intnum INT"
+            ");",
+        -1, &table, NULL));
+        EXPECT_EQ(SQLITE_DONE, sqlite3_step(table));
+        EXPECT_EQ(SQLITE_OK, sqlite3_finalize(table));
+    }
+
     {
         /* insert */
         sqlite3_stmt* insert;
-        sqlite3_prepare_v2(db, "INSERT INTO Persons VALUES (?, ?, ?, ?, ?)", 1024, &insert, NULL);
-        sqlite3_bind_int(insert, 1, 42);
-        sqlite3_bind_text(insert, 2, "Chan", 255, NULL);
-        sqlite3_bind_text(insert, 3, "Jackie", 255, NULL);
-        sqlite3_bind_text(insert, 4, "Kowloon Park Drive 2, 86F", 255, NULL);
-        sqlite3_bind_text(insert, 5, "Hong Kong", 255, NULL);
-        sqlite3_step(insert);
-        sqlite3_finalize(insert);
+        EXPECT_EQ(SQLITE_OK,  sqlite3_prepare_v2(db, "INSERT INTO simple VALUES (null, 'sometext', 1.5, 2)", -1, &insert, NULL));
+        EXPECT_EQ(SQLITE_DONE, sqlite3_step(insert));
+        EXPECT_EQ(SQLITE_OK, sqlite3_finalize(insert));
     }
 
     {    
         /* read */
         sqlite3_stmt* read;
-        sqlite3_prepare_v2(db, "SELECT * FROM Persons WHERE P_Id=?", 1024, &read, NULL);
-        sqlite3_bind_int(read, 1, 42);
-        sqlite3_step(read);
+        EXPECT_EQ(SQLITE_OK, sqlite3_prepare_v2(db, "SELECT text, fpnum, intnum FROM simple WHERE id=1;", -1, &read, NULL));
+        EXPECT_EQ(SQLITE_ROW, sqlite3_step(read));
+        EXPECT_EQ("sometext", string((const char*) (sqlite3_column_text(read, 0))));
+        EXPECT_EQ(1.5, sqlite3_column_double(read, 1));
+        EXPECT_EQ(2, sqlite3_column_int(read, 2));
+        EXPECT_EQ(SQLITE_DONE, sqlite3_step(read));
         sqlite3_finalize(read);
     }
 
     {
         /* update */
         sqlite3_stmt* update;
-        sqlite3_prepare_v2(db, "UPDATE Persons SET FirstName=? WHERE P_Id=?", 1024, &update, NULL);
-        sqlite3_bind_text(update, 1, "Andrew", 255, NULL);
-        sqlite3_bind_int(update, 2, 42);
-        sqlite3_step(update);
-        sqlite3_finalize(update);
+        EXPECT_EQ(SQLITE_OK, sqlite3_prepare_v2(db, "UPDATE simple SET text=? WHERE id=?", -1, &update, NULL));
+        // Be careful, indexing into columns when reading starts with zero, but
+        // indices for binding arguments start with one!
+        EXPECT_EQ(SQLITE_OK, sqlite3_bind_text(update, 1, "someothertext", -1, NULL));
+        EXPECT_EQ(SQLITE_OK, sqlite3_bind_int(update, 2, 1));
+        EXPECT_EQ(SQLITE_DONE, sqlite3_step(update));
+        EXPECT_EQ(SQLITE_OK, sqlite3_finalize(update));
     }
 
     {
         /* delete */
         sqlite3_stmt* del;
-        sqlite3_prepare_v2(db, "DELETE FROM Persons WHERE P_Id=?", 1024, &del, NULL);
-        sqlite3_bind_int(del, 1, 42);
-        sqlite3_step(del);
-        sqlite3_finalize(del);
+        EXPECT_EQ(SQLITE_OK, sqlite3_prepare_v2(db, "DELETE FROM simple WHERE id=?", 1024, &del, NULL));
+        EXPECT_EQ(SQLITE_OK, sqlite3_bind_int(del, 1, 42));
+        EXPECT_EQ(SQLITE_DONE, sqlite3_step(del));
+        EXPECT_EQ(SQLITE_OK, sqlite3_finalize(del));
     }
 
-    sqlite3_close(db);
+    EXPECT_EQ(SQLITE_OK, sqlite3_close(db));
 }
 
-TEST(SqliteTest, CreateTable) {
-    sqlite3* db;
-    sqlite3_open("build/test.sqlite3", &db);
-    
-    {
-        // credits to http://www.w3schools.com/sql/sql_create_table.asp
-        sqlite3_stmt* create;
-        sqlite3_prepare_v2(db, "CREATE TABLE Persons2 "
-                "("
-                "    P_Id int,"
-                "    LastName varchar(255),"
-                "    FirstName varchar(255),"
-                "    Address varchar(255),"
-                "    City varchar(255)"
-                ")", 1024, &create, NULL);
-        sqlite3_step(create);   
-        sqlite3_finalize(create);
-    }
-
-    sqlite3_close(db);
-}
