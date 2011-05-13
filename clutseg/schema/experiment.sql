cfe3d2f903371c038d5eaacc8ab935082a3b8c62
Julius Adorf <jeadorf@gmail.com>
2011-05-13 12:58:05 +0200

    add id to experiment tablr

--- clutseg/schema/experiment.sql
+++ clutseg/schema/experiment.sql
@@ -1,5 +1,6 @@
 
 create table experiment (
+    id integer primary key autoincrement,
     paramset_id integer not null references paramset(id),
     response_id integer references response(id),
     train_set varchar(255) not null,
