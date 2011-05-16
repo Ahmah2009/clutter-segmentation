73e90048bad6ff3e502de48fb1d32d61d074707d
Julius Adorf <jeadorf@gmail.com>
2011-05-16 15:22:38 +0200

    macro disabling segfaulting PCLVisualizer

--- clutseg/test/test_map.cpp
+++ clutseg/test/test_map.cpp
@@ -123,21 +123,24 @@ TEST_F(Map, MapMarkers) {
     imshow("MapMarkers", markerImg);
     waitKey(-1);
 
-    // show the whole stuff
-    PCLVisualizer vis;
-    addPose(vis, PoseRT());
+    // TODO: PCLVisualizer segfaults for no reason...
+    #ifdef ENABLE_PCLVIS
+        // show the whole stuff
+        PCLVisualizer vis;
+        addPose(vis, PoseRT());
 
-    addMarkerPolygon3d(vis, red3d, 255, 0, 0, "red");
-    addMarkerPolygon3d(vis, green3d, 0, 216, 0, "green");
-    addMarkerPolygon3d(vis, yellow3d, 255, 216, 0, "yellow");
-    addMarkerPolygon3d(vis, blue3d, 0, 66, 255, "blue");
+        addMarkerPolygon3d(vis, red3d, 255, 0, 0, "red");
+        addMarkerPolygon3d(vis, green3d, 0, 216, 0, "green");
+        addMarkerPolygon3d(vis, yellow3d, 255, 216, 0, "yellow");
+        addMarkerPolygon3d(vis, blue3d, 0, 66, 255, "blue");
 
-    addMarkerPolygon3d(vis, red3d_hscaled, 204, 153, 153, "red_hscaled");
-    addMarkerPolygon3d(vis, green3d_hscaled, 153, 204, 153, "green_hscaled");
-    addMarkerPolygon3d(vis, yellow3d_hscaled, 204, 204, 153, "yellow_hscaled");
-    addMarkerPolygon3d(vis, blue3d_hscaled, 153, 153, 204, "blue_hscaled");
+        addMarkerPolygon3d(vis, red3d_hscaled, 204, 153, 153, "red_hscaled");
+        addMarkerPolygon3d(vis, green3d_hscaled, 153, 204, 153, "green_hscaled");
+        addMarkerPolygon3d(vis, yellow3d_hscaled, 204, 204, 153, "yellow_hscaled");
+        addMarkerPolygon3d(vis, blue3d_hscaled, 153, 153, 204, "blue_hscaled");
 
-    vis.addPointCloud(cloud);
-    vis.spin();
+        vis.addPointCloud(cloud);
+        vis.spin();
+    #endif 
 }
 
