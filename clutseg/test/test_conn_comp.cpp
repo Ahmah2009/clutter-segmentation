9116dddd890b7b54b3944ac9c9be569a6ab39924
Julius Adorf <jeadorf@gmail.com>
2011-05-20 14:39:11 +0200

    refactored test_conn_comp

--- clutseg/test/test_conn_comp.cpp
+++ clutseg/test/test_conn_comp.cpp
@@ -2,42 +2,39 @@
  * Author: Julius Adorf
  */
 
-#include "test.h"
+#include "clutseg/conn_comp.h"
 
 #include <gtest/gtest.h>
-#include <clutseg/conn_comp.h>
 #include <cv.h>
 #include <opencv2/highgui/highgui.hpp>
-#include <iostream>
-#include <algorithm>
-#include <stdint.h>
-#include <boost/format.hpp>
 
 using namespace cv;
 using namespace std;
 
-TEST(ConnComp, ReadMask) {
-    Mat image = imread("./data/mask.png", 0);
-    for (int i = 0; i < image.rows; i++) {
-        for (int j = 0; j < image.cols; j++) {
-            EXPECT_TRUE(image.at<uint8_t>(i, j) == 0 || image.at<uint8_t>(i, j) == 255);
+struct ConnCompTest : public ::testing::Test {
+
+    void SetUp() {
+        img = imread("./data/mask.png", 0);
+    }
+
+    Mat img;
+};
+
+
+TEST_F(ConnCompTest, ReadMask) {
+    for (int i = 0; i < img.rows; i++) {
+        for (int j = 0; j < img.cols; j++) {
+            EXPECT_TRUE(img.at<uint8_t>(i, j) == 0 || img.at<uint8_t>(i, j) == 255);
         }
     }
 }
 
-TEST(ConnComp, FillSmallConnectedComponents) {
-    Mat img = imread("data/mask.png", 0);
-
+TEST_F(ConnCompTest, FillSmallConnectedComponents) {
     ASSERT_FALSE(img.empty());
     img = img > 1;
-    namedWindow( "Source", 1 );
-    imshow( "Source", img);
-    
+    imshow("Source",img);
     largestConnectedComponent(img);
-
-    namedWindow( "Components", 1 );
-    imshow( "Components", img);
+    imshow("Components", img);
     waitKey(0);
-
 }
 
