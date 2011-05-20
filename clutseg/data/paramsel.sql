71e60d27e4323405dc0af409be35f8d87fc64c36
Julius Adorf <jeadorf@gmail.com>
2011-05-20 10:17:11 +0200

    change test param db to use ias_kinect_test_grounded_21

--- clutseg/data/paramsel.sql
+++ clutseg/data/paramsel.sql
@@ -7,5 +7,5 @@ insert into pms_guess (ransac_iterations_count, min_inliers_count, max_projectio
 insert into pms_fe (detector_type, extractor_type, descriptor_type, threshold, min_features, max_features, octaves) values ("FAST", "multi-scale", "rBRIEF", 40, null, null, 3);
 insert into pms_fe (detector_type, extractor_type, descriptor_type, threshold, min_features, max_features, octaves) values ("FAST", "multi-scale", "rBRIEF", 40, null, null, 3);
 insert into paramset (train_pms_fe_id, recog_pms_fe_id, detect_pms_match_id, detect_pms_guess_id, locate_pms_match_id, locate_pms_guess_id, recog_pms_clutseg_id) values (1, 2, 1, 1, 2, 2, 1);
-insert into experiment (paramset_id, response_id, train_set, test_set, sample_size, time, vcs_commit) values (1, null, "ias_kinect_train_v2", "ias_kinect_test_grounded", 20, null, null); 
+insert into experiment (paramset_id, response_id, train_set, test_set, sample_size, time, vcs_commit) values (1, null, "ias_kinect_train_v2", "ias_kinect_test_grounded_21", 20, null, null); 
 
