6bf8035bce491ee06f6e3ff9dd1e287c0b24bab4
Julius Adorf <jeadorf@gmail.com>
2011-06-02 12:48:03 +0200

    corrected paths in result scripts

--- scripts/result-best-succ-rate.R
+++ scripts/result-best-succ-rate.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 library(RSQLite)
-con = dbConnect(dbDriver("SQLite"), dbname = paste(Sys.getenv("CLUTSEG_PATH"), "/clutter-segmentation/clutseg/build/paramsel.sqlite3", sep=""))
+con = dbConnect(dbDriver("SQLite"), dbname = Sys.getenv("CLUTSEG_EXPERIMENT_DB"))
 f = dbGetQuery(con, "select succ_rate from response order by succ_rate DESC limit 1")
 attach(f)
 png(paste(Sys.getenv("CLUTSEG_ARTIFACT_DIR"), "/best_succ_rate.png", sep=""))
--- scripts/result-roc.R
+++ scripts/result-roc.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 library(RSQLite)
-con = dbConnect(dbDriver("SQLite"), dbname = paste(Sys.getenv("CLUTSEG_EXPERIMENT_DB"), sep=""))
+con = dbConnect(dbDriver("SQLite"), dbname = Sys.getenv("CLUTSEG_EXPERIMENT_DB"))
 f = dbGetQuery(con, "select detect_fp_rate, detect_tp_rate from view_experiment_detect_roc")
 attach(f)
 png(paste(Sys.getenv("CLUTSEG_ARTIFACT_DIR"), "/detect_roc.png", sep=""))
