5fbed7093a68fdae07ab8dcb56c9adc38c949139
Julius Adorf <jeadorf@gmail.com>
2011-06-05 10:03:17 +0200

    improved binary synch'ing scripts

--- scripts/clutseg-binaries-rsync-down.bash
+++ scripts/clutseg-binaries-rsync-down.bash
@@ -1,4 +1,26 @@
 #!/usr/bin/env bash
 
 source $CLUTSEG_PATH/clutter-segmentation/scripts/base.bash $*
-rsync -avP municware.com:clutseg/* $CLUTSEG_PATH/
+rsync --recursive --archive municware.com:clutseg/ $CLUTSEG_PATH/
+
+trap "exit" INT TERM EXIT
+
+pushd $CLUTSEG_PATH > /dev/null
+    libs=$(ssh municware.com "find clutseg -type d -iname 'lib'")
+    bins=$(ssh municware.com "find clutseg -type d -iname 'bins'")
+
+    for lib in $libs ; do
+        printf "Downloading %-60s " $lib
+        mkdir -p $lib
+        rsync --relative --archive --recursive municware.com:clutseg/$lib/ $lib
+        echo "[OK]"
+    done
+
+    for bin in $bins ; do
+        printf "Downloading %-60s " $bin
+        mkdir -p $lib
+        rsync --relative --archive --recursive municware.com:clutseg/$bin/ $bin
+        echo "[OK]"
+    done
+
+popd > /dev/null
--- scripts/clutseg-binaries-rsync-up.bash
+++ scripts/clutseg-binaries-rsync-up.bash
@@ -2,17 +2,21 @@
 
 source $CLUTSEG_PATH/clutter-segmentation/scripts/base.bash $*
 
+trap "exit" INT TERM EXIT
+
 pushd $CLUTSEG_PATH > /dev/null
     libs=$(find . -type d -iname "lib")
     bins=$(find . -type d -iname "bin")
 
     for lib in $libs ; do
-        ssh municware.com "mkdir -p clutseg/$lib"
-        rsync -avP --delete $lib/* municware.com:clutseg/$lib
+        printf "Uploading %-60s " $lib
+        rsync --relative --archive --recursive --delete $lib municware.com:clutseg/
+        echo "[OK]"
     done
 
     for bin in $bins ; do
-        ssh municware.com "mkdir -p clutseg/$bin"
-        rsync -ravP --delete $bin/* municware.com:clutseg/$bin
+        printf "Uploading %-60s " $bin
+        rsync --relative --archive --recursive --delete $bin municware.com:clutseg/
+        echo "[OK]"
     done
 popd > /dev/null
