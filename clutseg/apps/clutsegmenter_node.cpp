a7ac215461084b7b0d005e18b21399fbedd6fe0e
Dejan Pangercic <dejan.pangercic@cs.tum.edu>
2011-07-08 00:24:35 -0700

    clutsegmenter node

--- clutseg/apps/clutsegmenter_node.cpp
+++ clutseg/apps/clutsegmenter_node.cpp
@@ -31,107 +31,172 @@
 #include <ros/ros.h>
 #include <sensor_msgs/image_encodings.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/Image.h>
 #include <std_msgs/String.h>
+#include <cv_bridge/CvBridge.h>
+
+//msg synchronisation
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
 
 using namespace std;
+using namespace message_filters;
+typedef message_filters::sync_policies::ApproximateTime< sensor_msgs::PointCloud2,
+							 sensor_msgs::Image > MySyncPolicy;
 
-int main(int argc, char **argv) {
-    ros::init(argc, argv, "clutsegmenter");
-    ros::NodeHandle n;
+class ClutterSegmenter
+{
+protected:
+  ros::NodeHandle n;
 
-    ros::Publisher inliers_publisher = n.advertise<sensor_msgs::PointCloud2>("clutseg_inliers", 1);
-    ros::Publisher pose_publisher = n.advertise<geometry_msgs::PoseStamped>("clutseg_pose", 1);
-    ros::Publisher object_publisher = n.advertise<std_msgs::String>("clutseg_object", 1);
+public:
+  ros::Publisher inliers_publisher;
+  ros::Publisher pose_publisher;
+  ros::Publisher object_publisher;
+  image_transport::Publisher hud_publisher;
+  std::string input_cloud_topic, input_image_topic, modelbase, camera_info; 
+  message_filters::Subscriber<sensor_msgs::Image> camera_sub_;
+  message_filters::Subscriber<sensor_msgs::PointCloud2> cloud_sub_;
+  message_filters::Synchronizer<MySyncPolicy> synchronizer_;
+  message_filters::Connection sync_connection_;
+  clutseg::PointCloudT query_cloud;
+  cv::Mat query_image;
+  cv::Mat query_image_bgr8;
+  sensor_msgs::CvBridge bridge;
+  clutseg::Clutsegmenter sgm;
+  ////////////////////////////////////////////////////////////////////////////////
+  ClutterSegmenter (ros::NodeHandle &n) : n(n),  synchronizer_( MySyncPolicy(1), cloud_sub_, camera_sub_)
+  {
+    inliers_publisher = n.advertise<sensor_msgs::PointCloud2>("clutseg_inliers", 1);
+    pose_publisher = n.advertise<geometry_msgs::PoseStamped>("clutseg_pose", 1);
+    object_publisher = n.advertise<std_msgs::String>("clutseg_object", 1);
 
     // This one is just for debugging purposes. It publishes a kind of heads-up display 
     // showing pose and inliers.
     image_transport::ImageTransport img_transp(n);
-    image_transport::Publisher hud_publisher = img_transp.advertise("clutseg_hud", 1);
+    hud_publisher = img_transp.advertise("clutseg_hud", 1);
 
-    // TODO: define subscriptions 
-    // ...
-    // ...
+    n.param("input_cloud_topic", input_cloud_topic, std::string("/kinect_head/camera/rgb/points"));
+    n.param("input_image_topic", input_image_topic, std::string("/kinect_head/camera/rgb/image_color"));  
+    n.param("modelbase", modelbase, std::string("./data/orb.tar"));
+    n.param("camera_info", camera_info, std::string("./data/camera.yml"));
 
-    ros::Rate loop_rate(0.1);
+    // // synchronized subscriptions 
+    camera_sub_.subscribe( n, input_image_topic, 1000 );
+    cloud_sub_.subscribe( n, input_cloud_topic, 1000 );
+    sync_connection_ = synchronizer_.registerCallback( &ClutterSegmenter::callback, this );
 
-    // TODO: you need to place <modelbase>.tar somewhere
-    // TODO: you need to tell the node where to find <modelbase>.tar 
-    clutseg::Clutsegmenter sgm("./data/orb.tar", true);
+    sgm = clutseg::Clutsegmenter(modelbase, true);
     cv::namedWindow("hud");
+    ROS_INFO ("[ClutterSegmenter:] Constructor up");
+  }
 
-    while (ros::ok()) {
-        // 1. Read input from topics 
-        // --------------------------------------------------------------------
-        clutseg::PointCloudT query_cloud;
-        cv::Mat query_image;
-        cv::Mat query_image_bgr8;
+  ////////////////////////////////////////////////////////////////////////////////
+  void callback (const sensor_msgs::PointCloud2ConstPtr& pc, const sensor_msgs::ImageConstPtr& im)
+  {
+    //1. Reading input data
+    // get image
+    query_image = bridge.imgMsgToCv(im, "passthrough");
+    // get cloud
+    pcl::fromROSMsg (*pc, query_cloud);
+    
+    //TODO: check how to convert from CameraInfo Msg to opencv_candidate::Camera
+    // it shall work for Julius since the data is from the same sensor
+    opencv_candidate::Camera camera = opencv_candidate::Camera(camera_info, opencv_candidate::Camera::TOD_YAML);
 
-        // TODO: get query_image from subscribed topic, if color image is not
-        // available, grayscale is okay though debugging output will not be as nice.
-        query_image_bgr8 = cv::imread("./data/image_00000.png");
-        query_image_bgr8.convertTo(query_image, CV_8UC1);
-        // TODO: get query_cloud from subscribed topic
-        pcl::io::loadPCDFile("./data/cloud_00000.pcd", query_cloud);
-        // TODO: get camera from subscribed topic
-        opencv_candidate::Camera camera = opencv_candidate::Camera("./data/camera.yml", opencv_candidate::Camera::TOD_YAML);
+    //2. Recognizing
+    clutseg::Query query(query_image, query_cloud);
+    clutseg::Result result;
+    sgm.recognize(query, result);
+
+    // 3. Publish to topics
+    if (result.guess_made) 
+      { 
+	// 3.1. Publish inlier cloud
+	sensor_msgs::PointCloud2 inliers_msg;
+	clutseg::PointCloudT inliers = result.refine_choice.inlierCloud;
+	pcl::toROSMsg (inliers, inliers_msg);
+	inliers_publisher.publish(inliers_msg);
+
+	//3.2. Publish name of the recognized object
+	std_msgs::String object_msg;
+	object_msg.data = result.refine_choice.getObject()->name;
+	object_publisher.publish(object_msg);
+
+	// 3.3. Publish pose
+	geometry_msgs::PoseStamped pose_msg;
+	opencv_candidate::Pose pose = result.refine_choice.aligned_pose();
+	cv::Mat_<float> rmat = pose.r<cv::Mat_<float> >();
+	cv::Rodrigues(pose.r<cv::Mat_<float> >(), rmat);
+	Eigen::Matrix3f rotation_matrix;
+	cv::cv2eigen(rmat, rotation_matrix);
+	Eigen::Quaternion<float> quaternion(rotation_matrix);
+	pose_msg.header.frame_id = pc->header.frame_id;
+	pose_msg.pose.orientation.x = quaternion.x();
+	pose_msg.pose.orientation.y = quaternion.y();
+	pose_msg.pose.orientation.z = quaternion.z();
+	pose_msg.pose.orientation.w = quaternion.w();
+	pose_publisher.publish(pose_msg);
+      }
+
+    {
+      // Just for debugging
+      cv::Mat hud = query_image.clone();
+      if (result.guess_made) 
+	{
+	  clutseg::drawGuess(hud, result.refine_choice, camera, opencv_candidate::PoseRT());
+	}
+      cv_bridge::CvImage cv_hud;
+      //cv_hud.encoding = sensor_msgs::image_encodings::BGR8;
+      cv_hud.image = hud;
+      // cv::imshow("hud", cv_hud.image);
+      // cv::waitKey(-1);
+      hud_publisher.publish(cv_hud.toImageMsg());
+    } 
+  }
+};
+
+int main (int argc, char** argv)
+{
+  ros::init (argc, argv, "cloudsegmenter_node");
+  ros::NodeHandle n("~");
+  ClutterSegmenter cs(n);
+  ros::spin ();
+  return (0);
+}
+
+
+// void callback (const sensor_msgs::ImageConstPtr& im, const sensor_msgs::PointCloud2ConstPtr& pc)
+// {
+
+// }
+
+// int main(int argc, char **argv) 
+// {
+//     ros::init(argc, argv, "clutsegmenter");
+//     ros::NodeHandle n ("~");
+
+
+
+//     ros::Rate loop_rate(0.1);
+
+
+
+// while (ros::ok()) {
 
         // 2. Recognize
         // --------------------------------------------------------------------
-        clutseg::Query query(query_image, query_cloud);
-        clutseg::Result result;
-        sgm.recognize(query, result);
 
-        // 3. Publish to topics
-        // --------------------------------------------------------------------
+
+//         
+//         // --------------------------------------------------------------------
        
-        if (result.guess_made) { 
-            // 3.1. Publish inlier cloud
-            sensor_msgs::PointCloud2 inliers_msg;
-            clutseg::PointCloudT inliers = result.refine_choice.inlierCloud;
-            // TODO: fill inliers_msg with inliers point cloud
-            inliers_publisher.publish(inliers_msg);
-
-            // 3.2. Publish name of the recognized object
-            std_msgs::String object_msg;
-            object_msg.data = result.refine_choice.getObject()->name;
-            object_publisher.publish(object_msg);
-
-            // 3.3. Publish pose
-            geometry_msgs::PoseStamped pose_msg;
-            opencv_candidate::Pose pose = result.refine_choice.aligned_pose();
-            cv::Mat_<float> rmat = pose.r<cv::Mat_<float> >();
-            cv::Rodrigues(pose.r<cv::Mat_<float> >(), rmat);
-            Eigen::Matrix3f rotation_matrix;
-            cv::cv2eigen(rmat, rotation_matrix);
-            Eigen::Quaternion<float> quaternion(rotation_matrix);
-            // TODO: what to fill into frame_id??
-            pose_msg.header.frame_id = "/openni_camera";
-            pose_msg.pose.orientation.x = quaternion.x();
-            pose_msg.pose.orientation.y = quaternion.y();
-            pose_msg.pose.orientation.z = quaternion.z();
-            pose_msg.pose.orientation.w = quaternion.w();
-            pose_publisher.publish(pose_msg);
-        }
  
-        {
-            // Just for debugging
-            cv::Mat hud = query_image_bgr8.clone();
-            if (result.guess_made) {
-                clutseg::drawGuess(hud, result.refine_choice, camera, opencv_candidate::PoseRT());
-            }
 
-            cv_bridge::CvImage cv_hud;
-            cv_hud.encoding = sensor_msgs::image_encodings::BGR8;
-            cv_hud.image = hud;
 
-            // cv::imshow("hud", cv_hud.image);
-            // cv::waitKey(-1);
-
-            hud_publisher.publish(cv_hud.toImageMsg());
-        } 
-
-        ros::spinOnce();
-        loop_rate.sleep();
-    }
-}
+//         ros::spinOnce();
+//         loop_rate.sleep();
+//     }
+// }
 
