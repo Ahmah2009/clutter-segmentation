f07c5910ad1a55e5a27f5057abcf7fa13968a6d4
Julius Adorf <jeadorf@gmail.com>
2011-07-08 09:39:06 +0200

    add pnp_ransac from vslam/posest to list of modified files

---
+++ clutseg/test/test_pnp_ransac.cpp
@@ -0,0 +1,47 @@
+/**
+ * Author: Julius Adorf
+ */
+
+#include "test.h"
+
+#include <gtest/gtest.h>
+
+#include <ctime>
+#include <cv.h>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+class test_pnp_ransac : public ::testing::Test {
+    public:
+        virtual void SetUp() {
+        }
+};
+
+/** Check whether the random number generator from OpenCV
+ * requires manual initialization of the seed in order to
+ * make it appear non-deterministic. */
+TEST_F(test_pnp_ransac, rng_deterministic) {
+    RNG rng;
+    RNG rng2;
+    cout << rng() << endl;
+    cout << rng2() << endl;
+    EXPECT_EQ(rng(), rng2());
+    EXPECT_EQ(rng(), rng2());
+    EXPECT_EQ(rng(), rng2());
+}
+
+/** Check whether initializing the seed gives the desired
+ * pseudo-non-deterministic behaviour. */
+TEST_F(test_pnp_ransac, rng_initialized_seed) {
+    RNG rng;
+    RNG rng2;
+    rng2.state = clock();
+    cout << rng() << endl;
+    cout << rng2() << endl;
+    EXPECT_NE(rng(), rng2());
+    EXPECT_NE(rng(), rng2());
+    EXPECT_NE(rng(), rng2());
+}
+
