be5efe51f7939e7ba39a5b21932975736fb7e5c2
Julius Adorf <jeadorf@gmail.com>
2011-07-09 17:32:40 +0200

    obsoleted: migration script for experiment database

--- scripts/rename_orb.sql
+++
@@ -1,2 +0,0 @@
-update pms_fe set detector_type="ORB-TOD", descriptor_type="ORB-TOD", extractor_type="ORB-TOD" where detector_type="ORB" and descriptor_type="ORB" and extractor_type="ORB";
-update pms_fe set detector_type="ORB", descriptor_type="ORB", extractor_type="ORB" where detector_type="ORB-OpenCV" and descriptor_type="ORB-OpenCV" and extractor_type="ORB-OpenCV";
