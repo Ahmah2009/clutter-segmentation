fd5bc5945a8c61f027aa29b8ab507cc912144cce
Julius Adorf <jeadorf@gmail.com>
2011-07-21 21:55:16 +0200

    rename installation script; preliminarily remove test run

--- ctags.bash
+++ ctags.bash
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-# Creates tags for diamondback sources, clutter-segmentation, stack
-# object_recognition, and dependencies of tod_training and tod_detecting.
 if [ ! "$CLUTSEG_PATH" ] ; then
     echo "ERROR: Environment variable CLUTSEG_PATH is not defined."
     exit
